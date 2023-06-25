70632b1cf1de30115251521275e40272c969290f
Ronald Perez <rpere034@gmail.com>
2023-06-25 02:12:48 -0600

    completed tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) 
+{
+  let celsius = (fahrenheit-32.0)*(5/9);
+
+  return parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) 
+{
+  let fahrenheit = celsius*(9.0/5) +32.0;
+
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
