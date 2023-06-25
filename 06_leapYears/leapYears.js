ba994ddfe64b115eed9c98f95fef62303ab2e4a4
Ronald Perez <rpere034@gmail.com>
2023-06-25 01:45:38 -0600

    complete leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,24 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    // need to do validation first, return ERROR if <0 or not a number
+    if (year < 0 || !Number.isInteger(year))
+    {
+        return "ERROR";
+    }
+
+    if(year%4 === 0)
+    {
+        if ((year%100 === 0) && !(year%400 === 0))
+        {
+            return false;
+        } else
+        {
+            return true;
+        }
+    } else
+    {
+        return false;
+    }
 };
 
 // Do not edit below this line
