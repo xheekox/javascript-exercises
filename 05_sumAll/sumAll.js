42aa1f6c712b50096612c47c306db71327d3e1c9
Ronald Perez <rpere034@gmail.com>
2023-06-25 01:25:02 -0600

    sumall completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) 
+{
+    let beginningInt;
+    let endingInt;
 
+    // check if values are not negative or that they are a number.
+    if (int1<0 || int2<0 || !Number.isInteger(int1) || !Number.isInteger(int2))
+    {
+        return "ERROR";
+    } else if (int1 < int2)
+    {
+        beginningInt = int1;
+        endingInt = int2;
+    } else 
+    {
+        beginningInt = int2;
+        endingInt = int1;
+    }
+
+    let finalSum = 0;
+
+    for (let i = beginningInt; i <= endingInt; i++)
+    {
+        finalSum += i;
+    }
+
+    return finalSum;
 };
 
 // Do not edit below this line
