04ccb5bf76f66b5516221b66f50d458ef28ae2e9
Ronald Perez <rpere034@gmail.com>
2023-07-08 11:53:25 -0600

    completed getthetitles problem

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,23 @@
-const getTheTitles = function() {
+// const books = [
+//     {
+//         title: "book of the dead",
+//         author: "vlad putin",
+//     },
+//     {
+//         title: "brave new world!",
+//         author: "Alduos Huxley",
+//     }
+// ]
+
+// console.log(books.map(element => element.title));
+
+const getTheTitles = function(listOfBooks) {
+
+    // array [ {key1:property1, key2:property2}, ...]
+    // each array element, is an object with two key pairs.
+    // iterate through to array
+    // access the title or
+    return listOfBooks.map(element => element.title);
 
 };
 
